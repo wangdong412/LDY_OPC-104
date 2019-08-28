e155fad7b91ce374ec3a6cc181f3bd6c9af93d1b
wangdong412 <wangdong412@163.com>
2019-08-28 09:27:07 +0800

    delete

--- worksolution/hw_Common/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// hw_Common.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
